06a026942f18dd5944650a342e6668564da2ca9f
Tariq Khawaja <tariqkhawaja26@live.com>
2022-09-11 21:58:14 +0100

    updated timetable

--- src/components/TimetableSection/index.js
+++ src/components/TimetableSection/index.js
@@ -15,8 +15,8 @@ const TimetableSection = () => {
                 <TimetableContent>
                     <TopLine>Timetable</TopLine>
                     <ImgWrap>
-                        <Img src={require('../../Assets/Images/Timetable2.jpg')} alt=
-                            'Timetable, Monday Gi Fundamentals wih Aaron: 18:30-20:00, Tuesday, Submission Grappling with Pedro: 18:30-20:00, Thursday Gi Fundamentals with Tom: 18:30-20:00, Friday Gi Fundamentals with Tom: 18:15-19:15, Gi Intermediate with Aaron: 19:15-20:30, Sunday Open Mat: 11:00-13:00' />
+                        <Img src={require('../../Assets/Images/Timetable3.jpeg')} alt=
+                            'Timetable, Monday Gi Fundamentals wih Aaron: 18:30-20:00, Tuesday, Submission Grappling with Pedro: 18:30-20:00, Wednesday Gi Fundamentals: 11:00-12:00, Thursday Gi Fundamentals with Tom: 18:30-20:00, Friday Gi Fundamentals with Tom: 18:15-19:15, Gi Intermediate with Aaron: 19:15-20:30, Sunday Open Mat: 11:00-13:00' />
                     </ImgWrap>
                     <Footer />
                 </TimetableContent>
