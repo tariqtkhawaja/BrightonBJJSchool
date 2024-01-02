dbeb68bbb5fd4c096a7e1a51380fefb063fb8b3e
Tariq Khawaja <68391372+Tariqk26@users.noreply.github.com>
2024-01-02 23:18:10 +0000

    New timetable & Prices 2024 (#8)
    * update Timetable
    
    * update prices
    
    ---------
    
    Co-authored-by: Tariq <tariq.khawaja@cyclr.com>

--- src/components/TimetableSection/index.js
+++ src/components/TimetableSection/index.js
@@ -15,8 +15,8 @@ const TimetableSection = () => {
                 <TimetableContent>
                     <TopLine>Timetable</TopLine>
                     <ImgWrap>
-                        <Img src={require('../../Assets/Images/Timetable8.jpeg')} alt=
-                            'Timetable, Monday Beginners Only with Aaron: 18:00-19:00 Gi Fundamentals wih Aaron: 19:00-20:30, Tuesday, Submission Grappling with Pedro/Aaron: 18:30-20:00, Wednesday Gi Fundamentals with Tom: 11:00-12:00,  Beginners only with Anjelika 18:00-19:30, No-Gi with Dino 19:40-21:00, Thursday Gi Fundamentals with Tom: 18:30-20:00, Friday Gi Intermediate with Aaron: 18:15-19:15, Saturday Wrestling with Dino: 12:00 - 13:00,  Sunday Open Mat: 11:00-13:00' />
+                        <Img src={require('../../Assets/Images/Timetable9.jpeg')} alt=
+                            'Timetable, Monday Gi Takedown Class with Aaron: 11:00-12:00 Beginners Only with Aaron: 18:00-19:00 Gi Fundamentals wih Aaron: 19:00-20:30, Tuesday, Submission Grappling with Pedro/Aaron: 18:30-20:00, Wednesday Gi Fundamentals with Tom: 11:00-12:00,  Beginners only with Anjelika 18:00-19:30, No-Gi with Dino 19:40-21:00, Thursday Gi Fundamentals with Tom: 18:30-20:00, Friday Gi Intermediate with Aaron: 18:15-19:15, Saturday Wrestling with Dino: 12:00 - 13:00,  Sunday Open Mat: 11:00-13:00' />
                     </ImgWrap>
                     <Footer />
                 </TimetableContent>
